3f270f6c51fd33a65fe21d66007ac5a58b49a160
ProjectCoreDevs <projectcore.devs@gmail.com>
2016-02-24 22:37:25 +0100

    add config description

--- admin/functions/config.php
+++ admin/functions/config.php
@@ -2,29 +2,24 @@
 /********************************************
 ************Database configuration***********
 ********************************************/
-$DB_HOST = '127.0.0.1';
-$DB_USERNAME = 'root';
-$DB_PASSWORD = 'gogo2012';
+$DB_HOST = '127.0.0.1';  // your host address
+$DB_USERNAME = 'root';   // your username
+$DB_PASSWORD = 'accent'; // your password
 
-$DB_WEBSITE = 'darkcore';
-$DB_WORLD = 'world';
-$DB_AUTH = 'auth';
-$DB_CHARACTERS = 'characters';
+$DB_WEBSITE = 'darkcore';  		// Website database
+$DB_WORLD = 'world';       		// World table of trinitycore
+$DB_AUTH = 'auth';         		// Auth table of trinitycore
+$DB_CHARACTERS = 'characters';	// Character table of trinitycore
 
 /********************************************
 ************Admin configuration************
 ********************************************/
 
-$adminSecurityPass = 'test35';
+$adminSecurityPass = 'test35'; // you can modify this password, it's the security password in the admin login page !
+
 $warningError0 = 'ERROR: Your password and username doesn\'t match !';
 $warningError1 = 'ERROR: Your rank isn\'t the admin rank ! The main admin has been warned !';
 $warningError2 = 'ERROR: The admin code is wrong or required, without, you can\'t connect to this part of the website !';
 $warningError3 = 'ERROR: Please enter the username, password and the admin code, without, you can\'t connect to this part of the site !';
-
-/********************************************
-************Website configuration************
-********************************************/
-$websiteTitle = 'Website Beta Title';
-$realmPortal = '127.0.0.1';
 
 ?>
