4db87a5943f9f1391cfb3b25d04cd9c959b99dcd
ProjectCoreDevs <projectcore.devs@gmail.com>
2016-02-24 22:32:41 +0100

    Create admin zone (work in progress)

---
+++ admin/functions/config.php
@@ -0,0 +1,30 @@
+<?php
+/********************************************
+************Database configuration***********
+********************************************/
+$DB_HOST = '127.0.0.1';
+$DB_USERNAME = 'root';
+$DB_PASSWORD = 'gogo2012';
+
+$DB_WEBSITE = 'darkcore';
+$DB_WORLD = 'world';
+$DB_AUTH = 'auth';
+$DB_CHARACTERS = 'characters';
+
+/********************************************
+************Admin configuration************
+********************************************/
+
+$adminSecurityPass = 'test35';
+$warningError0 = 'ERROR: Your password and username doesn\'t match !';
+$warningError1 = 'ERROR: Your rank isn\'t the admin rank ! The main admin has been warned !';
+$warningError2 = 'ERROR: The admin code is wrong or required, without, you can\'t connect to this part of the website !';
+$warningError3 = 'ERROR: Please enter the username, password and the admin code, without, you can\'t connect to this part of the site !';
+
+/********************************************
+************Website configuration************
+********************************************/
+$websiteTitle = 'Website Beta Title';
+$realmPortal = '127.0.0.1';
+
+?>
