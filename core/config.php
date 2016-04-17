95585842a939241518d23dd5032bda0894dde618
bragib <m.kozubov@yandex.ru>
2016-04-18 01:44:39 +0300

    Config

--- core/config.php
+++ core/config.php
@@ -10,7 +10,7 @@ $GLOBALS['DB_CHARACTERS'] = 'characters';	// Characters database name of trinity
 $GLOBALS['DB_WORLD'] = 'world';				// World database name of trinitycore 6x
 
 function connect($host,$username,$password){
-	$con = mysqli_connect('127.0.0.1','root',''); // 'Host', 'username', 'password'
+	$con = mysqli_connect('127.0.0.1','trinity','trinity'); // 'Host', 'username', 'password'
 	if (!$con) {
 		printf("Connect failed: %s\n", mysqli_connect_error());
 		exit();
